9ff78af1b917c94e95f3be5c21dd5f0c577fc32d
droksty <droksty@gmail.com>
2023-11-14 20:16:41 +0200

    login screen fxml and css

--- src/main/java/io/droksty/bankappfx/App.java
+++ src/main/java/io/droksty/bankappfx/App.java
@@ -1,12 +1,21 @@
 package io.droksty.bankappfx;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class App extends Application {
     @Override
-    public void start(Stage stage) {
-
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("BankAppFX");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/io/droksty/bankappfx/controller/LoginController.java
+++ src/main/java/io/droksty/bankappfx/controller/LoginController.java
@@ -1,4 +1,15 @@
 package io.droksty.bankappfx.controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public ChoiceBox accountSelector;
+    public Label usernameLabel;
+    public TextField usernameField;
+    public TextField passwordField;
+    public Button loginButton;
+    public Label errorLabel;
 }
