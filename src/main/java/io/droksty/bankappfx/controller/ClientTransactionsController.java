23dc0b36e7bcb703074769ec4e96f4e9039660ea
droksty <droksty@gmail.com>
2023-11-17 20:03:18 +0200

    add client-transactions fxml, css and controller files

---
+++ src/main/java/io/droksty/bankappfx/controller/ClientTransactionsController.java
@@ -0,0 +1,16 @@
+package io.droksty.bankappfx.controller;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientTransactionsController implements Initializable {
+    public ListView transactionsListView;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
