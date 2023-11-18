9597a592c2907641820d43038fc483a066f61007
droksty <droksty@gmail.com>
2023-11-18 07:56:08 +0200

    add client accounts fxml, css and controller files

---
+++ src/main/java/io/droksty/bankappfx/controller/ClientAccountsController.java
@@ -0,0 +1,20 @@
+package io.droksty.bankappfx.controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ClientAccountsController {
+    public Label checkingAccNumber;
+    public Label checkingAccLimit;
+    public Label checkingAccCreationDate;
+    public Label checkingAccBalance;
+    public Label SavingsAccNum;
+    public Label savingsAccLimit;
+    public Label savingsAccCreationDate;
+    public Label savingsAccBalance;
+    public TextField amountToTransferToSavings;
+    public Button transferToSavingsButton;
+    public TextField amountToTransferToChecking;
+    public Button transferToCheckingButton;
+}
