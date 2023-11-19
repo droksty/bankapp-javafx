fa479f7b320006123370ef9e17ada1bd36c74e89
droksty <droksty@gmail.com>
2023-11-19 13:31:53 +0200

    add admin-client-list fxml, styl and controller files

---
+++ src/main/java/io/droksty/bankappfx/controller/admin/AdminClientListController.java
@@ -0,0 +1,9 @@
+package io.droksty.bankappfx.controller.admin;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class AdminClientListController {
+    @FXML
+    public ListView clientListView;
+}
