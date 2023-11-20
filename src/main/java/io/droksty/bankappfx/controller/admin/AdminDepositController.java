40c0c9ff0e4ef9bd1c83e2582a818f3fb8fdb005
droksty <droksty@gmail.com>
2023-11-20 10:15:11 +0200

    add admin deposit view fxml, style and controller files

---
+++ src/main/java/io/droksty/bankappfx/controller/admin/AdminDepositController.java
@@ -0,0 +1,19 @@
+package io.droksty.bankappfx.controller.admin;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class AdminDepositController {
+    @FXML
+    private TextField payeeHandleField;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private ListView resultListView;
+    @FXML
+    private TextField amountField;
+    @FXML
+    private Button depositButton;
+}
