599338c96fb45a23f2917793366843659d68362b
droksty <droksty@gmail.com>
2023-11-20 18:10:19 +0200

    add client cell fxml, css and controller class for admin clietnList view

---
+++ src/main/java/io/droksty/bankappfx/controller/admin/ClientCellController.java
@@ -0,0 +1,24 @@
+package io.droksty.bankappfx.controller.admin;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ClientCellController {
+    @FXML
+    private Label firstnameLabel;
+    @FXML
+    private Label lastnameLabel;
+    @FXML
+    private Label userHandleLabel;
+    @FXML
+    private Label checkingAccNumLabel;
+    @FXML
+    private Label savingsAccNumLabel;
+    @FXML
+    private Label dateLabel;
+    @FXML
+    private Button updateButton;
+    @FXML
+    private Button deleteButton;
+}
