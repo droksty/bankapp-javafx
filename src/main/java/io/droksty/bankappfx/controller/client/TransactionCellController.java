d72284abad4beef29130142141c351b23d605948
droksty <droksty@gmail.com>
2023-12-01 00:34:19 +0200

    Add transaction icons logic

--- src/main/java/io/droksty/bankappfx/controller/client/TransactionCellController.java
+++ src/main/java/io/droksty/bankappfx/controller/client/TransactionCellController.java
@@ -1,9 +1,11 @@
 package io.droksty.bankappfx.controller.client;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import io.droksty.bankappfx.model.Model;
 import io.droksty.bankappfx.model.transaction.Transaction;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 public class TransactionCellController {
     @FXML
@@ -31,5 +33,16 @@ public class TransactionCellController {
         receiverLabel.textProperty().bind(transaction.receiverProperty());
         amountLabel.textProperty().bind(transaction.amountProperty().asString());
         datetimeLabel.textProperty().bind(transaction.dateProperty().asString());
+        setTransactionIcons();
+    }
+
+    private void setTransactionIcons() {
+        if (transaction.senderProperty().get().equals(Model.getInstance().getClient().userHandleProperty().get())) {
+            incomingIcon.setVisible(false);
+            outcomingIcon.setFill(Color.RED);
+        } else {
+            outcomingIcon.setVisible(false);
+            incomingIcon.setFill(Color.GREEN);
+        }
     }
 }
